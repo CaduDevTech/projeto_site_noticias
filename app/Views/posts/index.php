df3c9c1427111a6635d400ec690b1e57ddd0546d
Cadu <dudueduardosantos2018@gmail.com>
2025-01-31 15:39:47 -0300

    Criação da tela de posts

---
+++ app/Views/posts/index.php
@@ -0,0 +1,14 @@
+    <div class="container py-5">
+
+<div class="card">
+    <div class="card-header bg-info text-white">POSTAGENS
+        <div class="float-right mt-1">
+            <a href= "<?=URL?>posts/cadastrar" class="btn btn-light">Escrever</a>
+        </div>
+        </div>
+        
+    <div class="card-body">
+        <P>Listar de posts</P>
+    </div>
+    </div>
+</div>
